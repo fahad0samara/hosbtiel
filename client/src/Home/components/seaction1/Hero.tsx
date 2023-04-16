523c6608e12d8d29a612b1a9d68e060b421a7eff
fahad <90055525+fahad0samara@users.noreply.github.com>
2023-04-16 20:53:05 +0300

    add

--- client/src/Home/components/seaction1/Hero.tsx
+++ client/src/Home/components/seaction1/Hero.tsx
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom'
 import { useLogIN } from '../../../../ContextLog'
 import img from '../../../assets/miracle.png'
 import img0 from '../../../assets/Hero.jpg'
+import { FaClipboardList } from 'react-icons/fa'
+import { RiUserHeartLine } from 'react-icons/ri'
 
 const Hero = () => {
  const { dark, logPatient, logAdmin } = useLogIN()
@@ -113,20 +115,14 @@ const Hero = () => {
       </div>
       <div className="mt-12 flex flex-col space-y-3 divide-gray-300 text-sm  sm:flex-row sm:space-y-0 sm:divide-x">
        <div className="flex max-w-xs space-x-2 px-4">
-        <svg
-         xmlns="http://www.w3.org/2000/svg"
-         className="h-12 w-12 text-emerald-600"
-         fill="none"
-         viewBox="0 0 24 24"
-         stroke="currentColor"
-         stroke-width="2"
-        >
-         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-         />
-        </svg>
+        <RiUserHeartLine className="h-12 w-12 text-yellow-400" />
+        <p>
+         <span className="font-bold">1,459</span> new patients
+         <span className=""> since last month</span>
+        </p>
+       </div>
+       <div className="flex max-w-xs space-x-2 px-4">
+        <FaClipboardList className="h-12 w-12 text-yellow-400" />
         <p>
          <span className="font-bold">1,459</span> new patients
          <span className=""> since last month</span>
